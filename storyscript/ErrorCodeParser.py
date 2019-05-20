25c97ec8a66b1ab5965ed2e74f0fe6aa36b39c8a
Rashmi Khanna <rashmi.khanna@in.ibm.com>
2019-05-20 18:37:31 +0530

    =changes for build pass

--- storyscript/ErrorCodeParser.py
+++ storyscript/ErrorCodeParser.py
@@ -1,19 +1,17 @@
 #!/usr/local/bin/python
-#import markdown
 import re
 import sys
 pattern = re.compile('(.*?)\s+\=\s+\((.*)?\)')
-def matchAndCreate(l, path_folder):
-    matchedObj = re.match(pattern, l)
-    group = matchedObj.group(2)
+def matchandcreate(l, path_folder):
+    matchedobj = re.match(pattern, l)
+    group = matchedobj.group(2)
     err = group.split(',')
-    errCode = err[0].strip("\''")
-    errMsg = err[1].strip("\''")
-    md_file = "{}/{}_metadata.md".format(path_folder, errCode)
+    errcode = err[0].strip("\''")
+    errmsg = err[1].strip("\''")
+    md_file = "{}/{}_metadata.md".format(path_folder, errcode)
     with open(md_file, 'w') as mdf:
-        #text = markdown.markdown("# "+errCode+errMsg)
         mdf.write('{} {}'.format(errCode, errMsg.strip('\""')))
-                                 
+                      
 def main():
     if not len(sys.argv) == 3:
         print('Please specify the output and input file')
@@ -21,37 +19,34 @@ def main():
         
     path_folder = sys.argv[1]
     input_file = sys.argv[2]
-    lines = []
-    foundStartLine = False
+    foundstartline = False
     #pattern = re.compile('(.*?)\s+\=\s+\((.*)?\)')
     compline= ''
-    with open(input_file, 'r') as f:
-        for line in f:
+    with open(input_file, 'r') as f :
+        for line in f :
             if line == '\n':
                 continue
-            if (( line.find("=")> -1) and (line.find(")") > -1) ):
+            if ((line.find('=') > -1) and (line.find(')') > -1)):
                 line = line.strip()
-                line = line.replace("\t+|\s+","")
-                #print('line is: '+line)
+                line = line.replace('\t+|\s+','')
                 if re.match(pattern, line):
-                    matchAndCreate(line, path_folder)
+                    matchandcreate(line, path_folder)
                 else:
                     print('full line not matches')
             elif (line.find("=")>-1):
-                #till you find end brace
-                foundStartLine = True
+                foundstartline = True
                 compline = compline + line.strip()
-            elif ( (line.find(')') > -1) and (foundStartLine == True )):
+            elif ( (line.find(')') > -1) and (foundstartline == True )):
                 compline = compline + line.strip()
-                compline = compline.replace("\t+|\s+","")
-                savedLine = compline
-                compline =''
-                foundStartLine = False
-                if re.match(pattern, savedLine):
-                    matchAndCreate(savedLine, path_folder)
+                compline = compline.replace('\t+|\s+','')
+                savedline = compline
+                compline = ''
+                foundstartline = False
+                if re.match(pattern, savedline):
+                    matchandcreate(savedline, path_folder)
                 else:
                     print('not matches')
-            elif ( foundStartLine == True):
+            elif ( foundstartline == True):
                 compline += line.strip()
             else:
                 continue
