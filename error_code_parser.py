009f2b5e020a1931b0f0b3542f6b6fd6aeac8988
Rashmi Khanna <k.rashmi.r@gmail.com>
2019-06-06 11:25:20 +0530

    =incorporate review comments

--- error_code_parser.py
+++ error_code_parser.py
@@ -1,18 +1,20 @@
-# !/usr/local/bin/python
+# !/usr/local/env python3
+"""
+Parse the error code to generate mdfiles
+"""
 import os
-import re
-PATTERN = re.compile(r'(.*?)\s+\=\s+\((.*)?\)')
+import argparse
 
 
 def matchandcreate(eachline, path_folder):
     """
       Parses each line and generates md file
     """
-    matchedobj = re.match(PATTERN, eachline)
-    group = matchedobj.group(2)
-    err = group.split(',')
-    errcode = err[0].strip("\''")
-    errmsg = err[1].strip("\''")
+    splitline = eachline.split('=')
+    #err = splitline[0]
+    errorline = splitline[1].split(',')
+    errcode = errorline[0][2:].strip("\''")
+    errmsg = errorline[1][:len(errorline[1])-1]
     md_file = '{}/{}_metadata.md'.format(path_folder, errcode)
     with open(md_file, 'w') as mdf:
         mdf.write('{} {}'.format(errcode, errmsg.strip('\""')))
@@ -24,33 +26,34 @@ def main():
       checks for lines matching regex to parse error codes.
       Created a folder mdfiles to generate the md files.
     """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("error_file", help="Specify the path to the error file")
+    args = parser.parse_args()
     path_folder = 'mdfiles'
     if not os.path.exists(path_folder):
         os.mkdir(path_folder)
-        print('Directory ', path_folder, 'created')
-    input_file = './storyscript/ErrorCodes.py'
+        print('Directory', path_folder, 'created')
+    input_file = args.error_file
     foundstartline = False
     compline = ''
-    with open(input_file, 'r') as file:
-        for line in file:
+    with open(input_file, 'r') as infile:
+        for line in infile:
             if line == '\n':
                 continue
-            if ((line.find('=') > -1) and (line.find(')') > -1)):
+            if (line.find('=') > -1) and (line.find(')') > -1):
                 line = line.strip()
                 line = line.replace(r'\t+|\s+', '')
-                if re.match(PATTERN, line):
-                    matchandcreate(line, path_folder)
+                matchandcreate(line, path_folder)
             elif line.find('=') > -1:
                 foundstartline = True
                 compline = compline + line.strip()
-            elif ((line.find(')') > -1) and foundstartline is True):
+            elif (line.find(')') > -1) and foundstartline is True:
                 compline = compline + line.strip()
                 compline = compline.replace(r'\t+|\s+', '')
                 savedline = compline
                 compline = ''
                 foundstartline = False
-                if re.match(PATTERN, savedline):
-                    matchandcreate(savedline, path_folder)
+                matchandcreate(savedline, path_folder)
             elif foundstartline is True:
                 compline += line.strip()
             else:
