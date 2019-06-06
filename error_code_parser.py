08bc9c3e6ac9f93a76fc44de648c2fc7e3da0776
Rashmi Khanna <k.rashmi.r@gmail.com>
2019-06-06 16:13:33 +0530

    =import file and remove all parsing

--- error_code_parser.py
+++ error_code_parser.py
@@ -2,21 +2,8 @@
 """
 Parse the error code to generate mdfiles
 """
-import argparse
 import os
-
-
-def matchandcreate(eachline, path_folder):
-    """
-      Parses each line and generates md file
-    """
-    splitline = eachline.split('=')
-    errorline = splitline[1].split(',')
-    errcode = errorline[0][2:].strip("\''")
-    errmsg = errorline[1][:len(errorline[1])-1]
-    md_file = '{}/{}_metadata.md'.format(path_folder, errcode)
-    with open(md_file, 'w') as mdf:
-        mdf.write('{} {}'.format(errcode, errmsg.strip('\""')))
+from storyscript.ErrorCodes import ErrorCodes
 
 
 def main():
@@ -25,39 +12,20 @@ def main():
       checks for lines matching regex to parse error codes.
       Created a folder mdfiles to generate the md files.
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('error_file', help='Specify path to the error file')
-    args = parser.parse_args()
     path_folder = 'mdfiles'
     if not os.path.exists(path_folder):
         os.mkdir(path_folder)
         print('Directory', path_folder, 'created')
-    input_file = args.error_file
-    foundstartline = False
-    compline = ''
-    with open(input_file, 'r') as infile:
-        for line in infile:
-            if line == '\n':
-                continue
-            if (line.find('=') > -1) and (line.find(')') > -1):
-                line = line.strip()
-                line = line.replace(r'\t+|\s+', '')
-                matchandcreate(line, path_folder)
-            elif line.find('=') > -1:
-                foundstartline = True
-                compline = compline + line.strip()
-            elif (line.find(')') > -1) and foundstartline is True:
-                compline = compline + line.strip()
-                compline = compline.replace(r'\t+|\s+', '')
-                savedline = compline
-                compline = ''
-                foundstartline = False
-                matchandcreate(savedline, path_folder)
-            elif foundstartline is True:
-                compline += line.strip()
-            else:
-                continue
-
+    ec = ErrorCodes()
+    members = [attr for attr in dir(ec) if not
+               callable(getattr(ec, attr)) and not attr.startswith("__")]
+    for error in members:
+        err = ec.get_error(error)
+        errcode = err[0]
+        errmsg = err[1]
+        md_file = '{}/{}_metadata.md'.format(path_folder, errcode)
+        with open(md_file, 'w') as mdf:
+            mdf.write('{} - {}'.format(errcode, errmsg))
 
 if __name__ == '__main__':
     main()
